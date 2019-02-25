2bf469b6f19df2a61612eda20399421967dde747
Felipe Correa <felipe.correa@gmail.com>
2019-02-24 23:25:43 -0300

    Removed old assets - Starting fresh

---
+++ Assets/Scripts/Actions/Attack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Actions
+{
+    public class Attack : MonoBehaviour
+    {
+        public GameObject handler;
+        public Sprite other;
+        private Animator anim;
+
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+            handler.GetComponent<SpriteRenderer>().sprite = other;
+        }
+        private void Update()
+        {
+            if(Input.GetKeyDown("space"))
+            {
+                anim.SetTrigger("attacking");
+            }
+        }
+
+        public void HasAttacked(object target)
+        {
+            Debug.Log("Attacked");
+        }
+    }
+}
---
+++ Assets/Scripts/Actions/AttackTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Actions
+{
+    public class AttackTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            SendMessageUpwards("HasAttacked", collision);
+        }
+    }
+}
