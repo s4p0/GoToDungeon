1427f463db2bf87549a73678a857ad35e0b66b53
Felipe Correa <felipe.correa@gmail.com>
2019-02-27 09:27:58 -0300

    changing progress to use Asset to define the level

--- Assets/Scripts/Level/LevelAsset.cs
+++ Assets/Scripts/Level/LevelAsset.cs
@@ -1,16 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using Assets.Scripts.Enemies;
+using Level;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Level
 {
+    [System.Serializable]
     [CreateAssetMenu(menuName = "GoToDungeon/New/Level")]
     public class LevelAsset : ScriptableObject
     {
         public int levelOrder;
-        public int levelName;
+        public string levelName;
         public int numberOfRooms;
         public int numberOfSmallMonsters;
         public int numberOfMediumMonsters;
         public int numberOfBigMonsters;
+
+        public Room[] rooms;
+        public Room[] aisles;
+
+        [Range(0,1)]
+        public float sidewaysProbability;
+
+        
+        public MonsterAsset[] monsters;
     }
 }
