3d584e5b3177d98bbb6fa5cb9184303e0c5336eb
Felipe Correa <felipe.correa@gmail.com>
2019-03-01 21:39:13 -0300

    health bar tests

---
+++ Assets/Scripts/UI/HealthBarControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class HealthBarControl : MonoBehaviour
+    {
+        [Range(0,1)]
+        public float scale = 1f;
+        private Vector3 current;
+
+        public GameObject foregroundSprite;
+
+
+        private void Start()
+        {
+            current = foregroundSprite.transform.localScale;
+        }
+        private void Update()
+        {
+            current.x = scale;
+            foregroundSprite.transform.localScale = current;
+        }
+    }
+}
